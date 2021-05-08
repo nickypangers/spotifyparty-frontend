3a86d860ea2b150a95778dec2fdb78bd22484a3a
nickypangers <nixonpang98@gmail.com>
2021-05-09 02:32:05 +0800

    websocket

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="room">
     <p>Room {{ $route.params.roomId }}</p>
+    <button @click.prevent="sendMessage('Hi sup')" v-show="connection != null">
+      Send to Server
+    </button>
   </div>
 </template>
 
@@ -11,5 +14,31 @@
 export default {
   name: "Room",
   components: {},
+  data() {
+    return {
+      connection: null,
+    };
+  },
+  created: function () {
+    console.log("Starting connection to WebSocket Server");
+    this.connection = new WebSocket(
+      "ws://localhost:3030/ws/" + this.$route.params.roomId
+    );
+    console.log("Attempting Websocket Connection");
+
+    this.connection.onopen = function (event) {
+      console.log(event);
+      console.log("Successfully connected to the websocket Server");
+    };
+
+    this.connection.onmessage = function (event) {
+      console.log(event);
+    };
+  },
+  methods: {
+    sendMessage: function (message) {
+      this.connection.send(message);
+    },
+  },
 };
 </script>
